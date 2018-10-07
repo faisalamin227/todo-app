cf1d90db143541bf707c787d1d2338f0dc57d28d
faisalamin227 <faisalamin227@gmail.com>
2018-10-07 13:19:15 +0300

    add initial files to repo

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddForm"
+
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: "buy some grocery" },
+      { id: 2, content: "throw away trash" }
+    ]
+  };
+
+  deleteTodo = (id) => {
+    console.log(id);
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    })
+    this.setState({
+      todos
+    })
+  }
+
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo]
+    this.setState({
+      todos
+    })
+  }
+
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
+      </div>
+    );
+  }
+}
+
+export default App;
